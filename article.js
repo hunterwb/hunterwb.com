3611c0334f50b29b23c2cc877be7c6bd0229c8b4
hunterwb <hunter@hunterwb.com>
2022-11-21 16:40:44 -0800

    Initial

---
+++ article.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var h1 = document.querySelector('h1').innerText;
+
+document.querySelector('.title').innerText = document.title;
+
+var links = document.querySelectorAll('a');
+for (var i = 0; i < links.length; i++) {
+	var link = links[i];
+	var t = link.getAttribute('href');
+	if (link.className === 'reflink') {
+		t = document.getElementById(t.substring(1)).innerText;
+	}
+	link.setAttribute('title', t);
+}
+
+var footer = '<footer><nav><ul>';
+[
+	'Albanian',
+	'Bopomofo',
+	'Deseret',
+	'Ethiopic',
+	'Kana',
+	'Lisu',
+	'Miao',
+	'Shavian',
+	'Syllabics'
+].forEach(function(t) {
+	if (t === h1) {
+		footer += '<li><b>' + t + '</b></li>';
+	} else {
+		var page = t.toLowerCase().replace(' ', '-') + '.html';
+		footer += '<li><a href="' + page + '">' + t + '</a></li>';
+	}
+});
+footer += '</ul></nav></footer>';
+document.querySelector('body').insertAdjacentHTML('beforeend', footer);
